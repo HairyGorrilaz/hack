03ba0f9824b3fa34ac0e88db98c219f6d885bdcf
Tzikas <niko.tzikas@gmail.com>
2019-10-04 09:43:32 -0400

    Updates

---
+++ precache-manifest.fc97567c4a1980c98a4cf7cf2a0be4bf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "454c1465a7192054dfa7482f8c819263",
+    "url": "/hack/index.html"
+  },
+  {
+    "revision": "5ad634e37790b57ab7d2",
+    "url": "/hack/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "23befe4542cadfd5747a",
+    "url": "/hack/static/js/2.ba1a6784.chunk.js"
+  },
+  {
+    "revision": "5ad634e37790b57ab7d2",
+    "url": "/hack/static/js/main.93ff766d.chunk.js"
+  },
+  {
+    "revision": "349295938b9cd72d8e10",
+    "url": "/hack/static/js/runtime-main.8c1d8df6.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/hack/static/media/logo.25bf045c.svg"
+  }
+]);
